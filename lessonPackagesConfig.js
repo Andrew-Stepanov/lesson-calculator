d2e3e3dab1e45d3d7afea73b8d1e05e91c7934d4
Andrew Stepanov <andrewstepanov@icloud.com>
2025-09-04 17:04:47 +0300

    Remove promotional banners from index.html and update bonus lesson configurations in lessonPackagesConfig.js

--- lessonPackagesConfig.js
+++ lessonPackagesConfig.js
@@ -6,10 +6,10 @@ const lessonPackagesConfig = {
                 8: { cost: 14400, link: "https://app.progkids.com/i/buy/eight-lessons-new", bonusLessons: 2, selected: false },
                 16: { cost: 27200, link: "https://app.progkids.com/i/buy/sixteen-lessons-new", bonusLessons: 3, selected: true },
                 24: { cost: 39600, link: "https://app.progkids.com/i/buy/twentyfour-lessons-new", bonusLessons: 3, selected: false },
-                32: { cost: 51200, link: "https://app.progkids.com/i/buy/thirtytwo-lessons-new", bonusLessons: 6, selected: true },
-                40: { cost: 62000, link: "https://app.progkids.com/i/buy/forty-lessons-new", bonusLessons: 7, selected: false },
-                48: { cost: 72000, link: "https://app.progkids.com/i/buy/fortyeight-lessons-new", bonusLessons: 8, selected: false },
-                56: { cost: 81200, link: "https://app.progkids.com/i/buy/fiftysix-lessons-new", bonusLessons: 10, selected: true }
+                32: { cost: 51200, link: "https://app.progkids.com/i/buy/thirtytwo-lessons-new", bonusLessons: 3, selected: true },
+                40: { cost: 62000, link: "https://app.progkids.com/i/buy/forty-lessons-new", bonusLessons: 4, selected: false },
+                48: { cost: 72000, link: "https://app.progkids.com/i/buy/fortyeight-lessons-new", bonusLessons: 4, selected: false },
+                56: { cost: 81200, link: "https://app.progkids.com/i/buy/fiftysix-lessons-new", bonusLessons: 5, selected: true }
             },
             level1: {
                 4: { cost: 5600, link: "https://app.progkids.com/i/buy/4800", bonusLessons: 0, selected: true },
